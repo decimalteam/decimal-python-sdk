8e599e4307bace97e5111a74f7b0054e4a8231d6
qoelk <qoelky@gmail.com>
2020-11-04 20:33:13 +0700

    upd: add some boilerplate classes

---
+++ decimal/transaction.py
@@ -0,0 +1,27 @@
+from .wallet import Wallet
+
+
+class Transaction:
+    def __init__(self, wallet: Wallet):
+        pass
+
+    def send_coins(self, **kwargs):
+        pass
+
+    def get_coin(self, **kwargs):
+        pass
+
+    def sell_coin(self, **kwargs):
+        pass
+
+    def create_coin(self, **kwargs):
+        pass
+
+    def validator_delegate(self, **kwargs):
+        pass
+
+    def validator_unbound(self, **kwargs):
+        pass
+
+    def validator_declare(self, **kwargs):
+        pass
---
+++ decimal/wallet.py
@@ -0,0 +1,9 @@
+class Wallet:
+    def __init__(self, mnemonic: str = None):
+        pass
+
+    def get_private_key(self) -> str:
+        pass
+
+    def get_public_key(self) -> str:
+        pass
