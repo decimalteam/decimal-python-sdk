55ced0410efec8ae78d38010bfab21ead407daf8
qoelk <qoelky@gmail.com>
2020-11-04 19:58:33 +0700

    upd: add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="decimal-python-sdk", # Replace with your own username
+    version="0.0.1",
+    author="DecimalTeam",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/decimalteam/decimal-python-sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
