24576f63da2b6c3e175d53c84dc4b38c0bff231c
Karl Kater <qoelky@gmail.com>
2020-11-17 17:20:14 +0700

    upd: generate seed, derive key

--- decimal/wallet.py
+++ decimal/wallet.py
@@ -1,11 +1,21 @@
 from mnemonic import Mnemonic
+from bip32 import BIP32
+import bech32
 
 
 class Wallet:
     def __init__(self, mnemonic: str = None):
+        mnemo = Mnemonic('english')
         if not mnemonic:
-            mnemonic = Mnemonic('english').generate()
+            mnemonic = mnemo.generate()
         self.mnemonic = mnemonic
+        # Generate and save seed
+        seed = mnemo.to_seed(mnemonic)
+        self._seed = seed
+        # Derive public key
+        bip32 = BIP32.from_seed(seed)
+        self._bip32 = bip32
+        self._public_key = bip32.get_xpub_from_path([0])
 
     def get_address(self) -> str:
         pass
@@ -14,4 +24,4 @@ class Wallet:
         pass
 
     def get_public_key(self) -> str:
-        pass
+        return self._public_key
