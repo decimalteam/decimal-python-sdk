f5958d12ec0b1f7e7d760a3d288d19e6cb4ee7ee
Karl Kater <qoelky@gmail.com>
2020-11-18 16:36:05 +0700

    upd: transactions boilerplate

--- decimal/api.py
+++ decimal/api.py
@@ -9,6 +9,8 @@ from transaction import Transaction
 class DecimalAPI:
     def __init__(self, base_url: str):
         self.base_url = base_url
+        if self.base_url[-1]:
+            self.base_url += '/'
 
     def get_coins_list(self):
         return self.__request('coin')
@@ -27,11 +29,8 @@ class DecimalAPI:
     def send_tx(self, tx: Transaction):
         return self.__request('', 'post', tx)
 
-    def test_request(self, path):
-        return self.__request(path)
-
     @staticmethod
-    def validate_address(address):
+    def validate_address(address: str):
         if len(address) != 41 or not address.startswith('dx'):
             raise Exception('Invalid address')
 
--- decimal/transaction.py
+++ decimal/transaction.py
@@ -2,5 +2,13 @@ from wallet import Wallet
 
 
 class Transaction:
-    def __init__(self, ):
+    def __init__(self, **kwargs):
         pass
+
+
+class BuyCoinTransaction(Transaction):
+    def __init__(self, coin_to_buy, amount, coin_to_sell, **kwargs):
+        self.coin_to_buy = coin_to_buy.upper()
+        self.amount = amount
+        self.coin_to_sell = coin_to_sell
+        super().__init__(**kwargs)
