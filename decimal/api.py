174789359e90e7ac8668119786733359a69a481f
Karl Kater <qoelky@gmail.com>
2020-11-18 16:14:23 +0700

    upd: some methods and address validation

--- decimal/api.py
+++ decimal/api.py
@@ -3,21 +3,42 @@ import json
 import requests
 from requests import Response
 
+from transaction import Transaction
+
 
 class DecimalAPI:
     def __init__(self, base_url: str):
         self.base_url = base_url
 
-    def get_coins_list(self) -> dict:
+    def get_coins_list(self):
         return self.__request('coin')
 
-    def __request(self, path: str, method: str = 'get') -> dict:
+    def get_coin(self, name: str):
+        return self.__request(f'coin/{name}')
+
+    def get_address(self, address: str):
+        self.validate_address(address)
+        return self.__request(f'address/{address}')
+
+    def get_stakes_by_address(self, address: str):
+        self.validate_address(address)
+        return self.__request(f'address/{address}/stakes')
+
+    def send_tx(self, tx: Transaction):
+        return self.__request('', 'post', tx)
+
+    def test_request(self, path):
+        return self.__request(path)
+
+    @staticmethod
+    def validate_address(address):
+        if len(address) != 41 or not address.startswith('dx'):
+            raise Exception('Invalid address')
+
+    def __request(self, path: str, method: str = 'get', payload=None):
         url = self.base_url + path
         if method == 'get':
             response = requests.get(url)
         else:
-            response = requests.post(url)
-        return self.__process_response(response)
-
-    def __process_response(self, response: Response) -> dict:
-        return json.loads(response.text)
+            response = requests.post(url, payload)
+        return response.text
