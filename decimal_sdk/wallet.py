d85993dcdae87d06f3915ecefaf4ad4acab9554f
Karl Kater <qoelky@gmail.com>
2020-11-19 18:16:06 +0700

    upd: generate address

--- decimal_sdk/wallet.py
+++ decimal_sdk/wallet.py
@@ -1,8 +1,9 @@
+from Cryptodome.Hash import SHA256, RIPEMD160
 from mnemonic import Mnemonic
 from bip32 import BIP32
 import bech32
 
-DERIVATION_PATH = "m/44'/60'/0'/0"
+DERIVATION_PATH = "m/44'/60'/0'/0/0"
 
 
 class Wallet:
@@ -19,12 +20,21 @@ class Wallet:
         bip32 = BIP32.from_seed(seed)
         self._public_key = bip32.get_xpub_from_path(DERIVATION_PATH)
         self._public_key_binary = bip32.get_pubkey_from_path(DERIVATION_PATH)
+        self.__generate_address()
 
     def get_address(self) -> str:
-        return bech32.encode('dx', 0, self._public_key_binary[2:])
+        return self._address
 
     def get_private_key(self) -> str:
         pass
 
     def get_public_key(self) -> str:
         return self._public_key
+
+    def __generate_address(self):
+        sha256_object = SHA256.new(self._public_key_binary)
+        sha256_hash = sha256_object.digest()
+        ripemd160_object = RIPEMD160.new(sha256_hash)
+        ripemd160_hash = ripemd160_object.digest()
+        address = bech32.bech32_encode('dx', bech32.convertbits(ripemd160_hash, 8, 5))
+        self._address = address
--- tests/wallet.py
+++ tests/wallet.py
@@ -1,12 +1,12 @@
 import unittest
 
-from decimal_sdk  import Wallet
+from decimal_sdk import Wallet
 
 
 class WalletTest(unittest.TestCase):
     def test_wallet_address_generation(self):
         wallet = Wallet('door mystery clever video cave balance fence general harbor mean then cheap system hover '
-                        'fitness hard lyrics sick energy length eyebrow crush bomb faculty ')
+                        'fitness hard lyrics sick energy length eyebrow crush bomb faculty')
         address = wallet.get_address()
         self.assertEqual('dx1puvhlmaf9tvttrmwrsdc8jqxnvtva2hnfde0ex', address)
 
