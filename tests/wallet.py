294dbff555f9a0f615112e66f444395b3e426bc8
Karl Kater <qoelky@gmail.com>
2020-11-19 18:35:05 +0700

    upd: refactor wallet test

--- tests/wallet.py
+++ tests/wallet.py
@@ -4,10 +4,19 @@ from decimal_sdk import Wallet
 
 
 class WalletTest(unittest.TestCase):
+    def setUp(self):
+        self.wallet = Wallet('door mystery clever video cave balance fence general harbor mean then cheap system hover '
+                             'fitness hard lyrics sick energy length eyebrow crush bomb faculty')
+
+    def test_wallet_keys_generation(self):
+        public_key = self.wallet.get_public_key()
+        self.assertEqual(
+            'xpub6GSdLvwGrd5dVPyh4immypMdXmF8mDno4GhABBC4H1rGDGkWJoxSvn'
+            'unTyTpCwegvPSU9KwnzFp9gjtQuEBA9KoKXQkBSPFHQDu4XW5gaso',
+            public_key)
+
     def test_wallet_address_generation(self):
-        wallet = Wallet('door mystery clever video cave balance fence general harbor mean then cheap system hover '
-                        'fitness hard lyrics sick energy length eyebrow crush bomb faculty')
-        address = wallet.get_address()
+        address = self.wallet.get_address()
         self.assertEqual('dx1puvhlmaf9tvttrmwrsdc8jqxnvtva2hnfde0ex', address)
 
 
