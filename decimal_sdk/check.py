67a2f0e965a3d16b170afd68acb9c13feff20fab
Egor Popov <ep.egor.popov@gmail.com>
2021-01-22 18:48:44 +0700

    checks almost ready

--- decimal_sdk/check.py
+++ decimal_sdk/check.py
@@ -1,4 +1,7 @@
 import hashlib
+import rlp
+import ethereum.transactions as crypto
+
 
 class Check:
     chain_id: str
@@ -12,34 +15,84 @@ class Check:
     s: int
 
     def get_sender(self):
-        return self.recover_plain(self.hash(), self.r, self.s, self.v)
+        return self.recover_plain(self.check_hash(), self.r, self.s, self.v)
 
     def lock_pub_key_(self):
-        pass
+        sig = self.lock.to_bytes()
+        if len(sig) < 65:
+            sig = 65-len(sig) + int(sig)
+
+        check_hash = self.hash_without_lock()
+        pub = crypto.ecrecover_to_pub(check_hash, sig)
+
+        if len(pub) == 0 or pub[0] != 4:
+            raise Exception("Invalid public key")
+
+        return pub
 
     def hash_without_lock(self):
-        pass
+        return self.rpl_hash(
+            [
+                self.chain_id,
+                self.coin,
+                self.amount,
+                self.nonce,
+                self.due_block
+            ]
+        )
 
-    def hash(self):
-        pass
+    def check_hash(self):
+        return self.rpl_hash(
+            [
+                self.chain_id,
+                self.coin,
+                self.amount,
+                self.nonce,
+                self.due_block,
+                self.lock
+            ]
+        )
 
     def hash_full(self):
-        pass
+        return self.rpl_hash(
+            [
+                self.chain_id,
+                self.coin,
+                self.amount,
+                self.nonce,
+                self.due_block,
+                self.lock,
+                self.v,
+                self.r,
+                self.s
+            ]
+        )
 
-    def sign(self):
-        pass
+    def sign(self, prvt_key):
+        h = self.check_hash()
+        sig = crypto.ecsign(h[:], prvt_key)
+        self.set_signature(sig)
 
     def set_signature(self, sig: bytearray):
         self.r = sig[0:32]
         self.s = sig[32:64]
-        self.v = sig[64] + 27
+        self.v = int((sig[64] + 27).to_bytes())
 
-    def parse_check(self):
+    def recover_plain(self, sighash, r, s, v): # todo
         pass
 
-    # def rpl_hash(self, x):
-    #     hw = hashlib.
-    #     hashlib.sha3_256
+    @staticmethod
+    def parse_check(buf):
+        check: Check = rlp.decode(buf)
+        if check.s == 0 or check.r == 0 or check.v == 0:
+            raise Exception("Incorrect tx signature")
 
-    def recover_plain(self, sighash, r, s, v):
-        pass
+        return check
+
+    @staticmethod
+    def rpl_hash(data):
+        return hashlib.sha3_256(data)
+
+    def __str__(self):
+        return f"Check sender: {self.get_sender()} nonce: {self.nonce}, " \
+               f"dueBlock: {self.due_block}, value: {self.amount} {self.coin}"
