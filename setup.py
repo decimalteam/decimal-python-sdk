695070d5c43c326b035af339cbe6f5ad7d6c1eee
SvyatS <prisenko2002@gmail.com>
2022-06-17 16:16:02 +0700

    feat: update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="decimal-python-sdk",
-    version="0.1.6",
+    version="0.1.7",
     author="DecimalTeam",
     description="",
     long_description=long_description,
